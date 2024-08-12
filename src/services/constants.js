3e249952d1f5ddf1c830e03d1ef7e9c8c06e81bb
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-08-12 23:03:10 +0700

    feat: Add public url of server

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,7 +1,9 @@
 const constants = {
-    LOGIN_URL: "http://localhost:5001/locket/login",
-    // LOGIN_URL: "https://lgprl5tl-5001.asse.devtunnels.ms/locket/login",
-    UPLOAD_MEDIA_URL: "http://localhost:5001/locket/upload-media",
+    // LOGIN_URL: "http://localhost:5001/locket/login",
+    LOGIN_URL: "https://lgprl5tl-5001.asse.devtunnels.ms/locket/login",
+    // UPLOAD_MEDIA_URL: "http://localhost:5001/locket/upload-media",
+    UPLOAD_MEDIA_URL:
+        "https://lgprl5tl-5001.asse.devtunnels.ms/locket/upload-media",
 };
 
 export default constants;
