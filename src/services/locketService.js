50fddfee690f17e31b0e57c2a2bb5380ea905c86
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-08-13 23:31:18 +0700

    feat: Migrate server to Render

--- src/services/locketService.js
+++ src/services/locketService.js
@@ -2,27 +2,40 @@ import axios from "axios";
 import constants from "./constants";
 import * as miscFuncs from "~/helper/misc-functions";
 
-export const login = async (email, password) => {
+export const login = async (email, password, onPleaseWait) => {
     try {
+        const timeOutId = setTimeout(() => {
+            onPleaseWait();
+        }, 5000);
+
         const res = await axios.post(constants.apiRoutes.LOGIN_URL, {
             email,
             password,
         });
 
+        clearTimeout(timeOutId);
         return res.data;
     } catch (error) {
         console.log("Login failed: ", error);
     }
 };
 
-export const uploadMedia = async (file, caption) => {
+export const uploadMedia = async (file, caption, onPleaseWait) => {
     try {
         const user = JSON.parse(miscFuncs.getCookie("user"));
         const formData = new FormData();
+
+        let timeOutId;
         if (file.type.includes("image")) {
             formData.append("images", file);
+            timeOutId = setTimeout(() => {
+                onPleaseWait();
+            }, 5000);
         } else if (file.type.includes("video")) {
             formData.append("videos", file);
+            timeOutId = setTimeout(() => {
+                onPleaseWait();
+            }, 10000);
         }
 
         formData.append("caption", caption);
@@ -39,6 +52,7 @@ export const uploadMedia = async (file, caption) => {
             },
         );
 
+        clearTimeout(timeOutId);
         return res.data;
     } catch (error) {
         console.log("Upload media failed: ", error);
