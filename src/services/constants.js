bef3ce5eb9825e2caffda27fb50182dac90f5f4b
Kirra <74497428+PhuNguyn@users.noreply.github.com>
2024-10-07 15:38:02 +0700

    Update constants.js

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,6 +1,6 @@
 const constants = {
     apiRoutes: {
-        LOGIN_URL: "https://locketuploader-be-render.onrender.com/locket/login",
+        LOGIN_URL: "https://locket-uploader-is6y5hg9z-phu-nguyens-projects-45aa87de.vercel.app//locket/login",
         UPLOAD_MEDIA_URL:
             "https://locketuploader-be-render.onrender.com/locket/upload-media",
     },
